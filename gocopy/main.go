2ecaa6f39a3ef5f8c7a03db9d671dfb6dc60ecdd
Alex <103932823+0Mediocrity@users.noreply.github.com>
2022-07-29 10:10:30 +0200

    Update main.go

--- gocopy/main.go
+++ gocopy/main.go
@@ -27,11 +27,6 @@ func main() {
 		//fmt.Println("Destination path not provided. Use the path %v"(), *pathfrom)
 		return
 	}
-	file, err := os.Open(*pathfrom)
-	if err != nil {
-		fmt.Println("Cannot open destination file:", *pathfrom)
-		return
-	}
 	if *overwrite == false {
 		file, err := os.Open(*pathto)
 		if err == nil {
@@ -40,13 +35,20 @@ func main() {
 			return
 		}
 	}
+	file, err := os.Open(*pathfrom)
+	if err != nil {
+		fmt.Println("Cannot open destination file:", *pathfrom)
+		return
+	}
 	var fsize int64 // so the compiler won't throw an error
 	if *limit == 0 {
+		fmt.Println("limit = 0", *limit)
 		fi, _ := file.Stat()
 		fsize = fi.Size()
 	} else {
 		fsize = *limit
 	}
+	//fmt.Println(int(fsize) / 4096)
 	file.Close()
 	Filecopy(*pathfrom, *pathto, fsize, *offset)
 
@@ -58,7 +60,7 @@ func Filecopy(pathfrom, pathto string, limit, offset int64) error {
 	// fmt.Println("From:", pathfrom)
 	// fmt.Println("To:", pathto)
 	// fmt.Println("offset", offset)
-	// fmt.Println("limit", limit)
+	//fmt.Println("limit", limit)
 	// fmt.Println("buffersize", *buffersize)
 
 	sourceFileStat, err := os.Stat(pathfrom)
@@ -81,23 +83,44 @@ func Filecopy(pathfrom, pathto string, limit, offset int64) error {
 	if err != nil {
 		panic(err)
 	}
-
 	buf := make([]byte, *buffersize)
 	bar := progressbar.New(int(limit))
+	maxIterations := limit / *buffersize
+	offsetIterations := offset / *buffersize
+	//fmt.Println(offsetIterations)
+	//file_iterations :=
+	var iterations int64
 	for {
+		//	fmt.Println(iterations, offsetIterations)
+		//	fmt.Println(iterations, max_iterations)
+		if iterations > maxIterations {
+			break
+		}
+		if iterations == maxIterations {
+			lastbuffer := limit - (maxIterations * *buffersize)
+			buf = make([]byte, lastbuffer)
+		}
 		n, err := source.Read(buf)
-		bar.Add(int(n))
-		fmt.Println(n)
+		//fmt.Println(iterations)
 		if err != nil && err != io.EOF {
 			return err
 		}
+		if offset == 0 || iterations > offsetIterations {
+			if _, err := destination.Write(buf[:n]); err != nil {
+				return err
+			}
+		}
+		if offset > 0 && iterations == offsetIterations {
+			pos := offset - offsetIterations**buffersize
+			if _, err := destination.Write(buf[pos:n]); err != nil {
+				return err
+			}
+		}
+		bar.Add(n)
 		if n == 0 {
 			break
 		}
-
-		if _, err := destination.Write(buf[:n]); err != nil {
-			return err
-		}
+		iterations++
 	}
 	return err
 }
@@ -105,10 +128,7 @@ func Filecopy(pathfrom, pathto string, limit, offset int64) error {
 /*
 Домашнее задание
 Копирование файлов
-Цель: Реализовать утилиту копирования файлов Утилита должна принимать следующие аргументы:
-* файл источник (From) * файл копия (To) * Отступ в источнике (Offset), по умолчанию - 0 * Количество копируемых байт (Limit), 
-по умолчанию - весь файл из From Выводить в консоль прогресс копирования в %, например с помощью github.com/cheggaaa/pb 
-Программа может НЕ обрабатывать файлы, у которых не известна длинна (например /dev/urandom).
+Цель: Реализовать утилиту копирования файлов Утилита должна принимать следующие аргументы * файл источник (From) * файл копия (To) * Отступ в источнике (Offset), по умолчанию - 0 * Количество копируемых байт (Limit), по умолчанию - весь файл из From Выводить в консоль прогресс копирования в %, например с помощью github.com/cheggaaa/pb Программа может НЕ обрабатывать файлы, у которых не известна длинна (например /dev/urandom).
 Завести в репозитории отдельный пакет (модуль) для этого ДЗ
 Реализовать функцию вида Copy(from string, to string, limit int, offset int) error
 Написать unit-тесты на функцию Copy
